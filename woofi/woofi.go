36cc921bf2dbf6aabd809073b2fb46b5585df116
Doan Phi Hung <davidhung@blep.ai>
2022-11-28 17:35:34 +0700

    Add additional fields

--- woofi/woofi.go
+++ woofi/woofi.go
@@ -2,11 +2,10 @@ package woofi
 
 import (
 	"context"
-	"fmt"
 	"time"
 
-	"github.com/nakji-network/connector"
-	"github.com/nakji-network/connector/common"
+	"github.com/nakji-network/connector/chain/ethereum"
+	"github.com/nakji-network/connector/kafkautils"
 
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -22,15 +21,13 @@ type Config struct {
 }
 
 type Connector struct {
-	*connector.Connector
 	*Config
-	sub       connector.ISubscription
+	*ethereum.Connector
 	contracts map[string]ISmartContract
 }
 
-func New(c *connector.Connector, config *Config) *Connector {
+func New(config *Config) *Connector {
 	return &Connector{
-		Connector: c,
 		Config:    config,
 		contracts: make(map[string]ISmartContract),
 	}
@@ -55,31 +52,56 @@ func (c *Connector) Start() {
 		addresses = append(addresses, ethcommon.HexToAddress(contract.Address()))
 	}
 
-	c.RegisterProtos(events...)
+	c.Connector = ethereum.NewConnector(context.Background(), addresses, c.NetworkName)
 
-	if sub, err := connector.NewSubscription(context.Background(), c.Connector, c.NetworkName, addresses, c.FromBlock, c.NumBlocks); err == nil {
-		c.sub = sub
-	} else {
-		log.Fatal().Err(err).Msg(fmt.Sprintf("%s connection error", c.NetworkName))
+	c.RegisterProtos(kafkautils.MsgTypeFct, events...)
+
+	c.Sub.Subscribe(context.Background())
+
+	if c.FromBlock > 0 || c.NumBlocks > 0 {
+		c.RegisterProtos(kafkautils.MsgTypeBf, events...)
+		go c.backfill(addresses)
 	}
 
 	for {
 		select {
-		case <-c.sub.Done():
+		case <-c.Sub.Done():
 			log.Info().Msg("connector shutdown")
 			return
 
 		//	Listen to error channel
-		case err := <-c.sub.Err():
+		case err := <-c.Sub.Err():
 			log.Error().Err(err).Str("network", c.NetworkName).Msg("subscription failed")
 			return
 
 		//	Listen to event logs
-		case vLog := <-c.sub.Logs():
-			if msg := c.parse(vLog); msg != nil {
-				c.EventSink <- msg
+		case vLog := <-c.Sub.Logs():
+			if msg := c.parse(vLog.Log); msg != nil {
+				c.EventSink <- &kafkautils.Message{
+					MsgType:  kafkautils.MsgTypeFct,
+					ProtoMsg: msg,
+				}
 			}
 		}
+	}
+}
+
+func (c *Connector) backfill(addresses []ethcommon.Address) {
+	logs := make(chan types.Log, 1000)
+	go func() {
+		for vLog := range logs {
+			if msg := c.parse(vLog); msg != nil {
+				c.EventSink <- &kafkautils.Message{
+					MsgType:  kafkautils.MsgTypeBf,
+					ProtoMsg: msg,
+				}
+			}
+		}
+		log.Info().Msg("backfill stopped")
+	}()
+	log.Info().Msg("backfill started")
+	if err := ethereum.BackfillFrom(context.Background(), c.Client, addresses, logs, c.FromBlock, c.NumBlocks); err != nil {
+		log.Error().Err(err).Msg("backfill failed")
 	}
 }
 
@@ -90,14 +112,34 @@ func (c *Connector) parse(vLog types.Log) protoreflect.ProtoMessage {
 		return nil
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
 	defer cancel()
 
-	t, err := c.sub.GetBlockTime(ctx, vLog)
+	ts, err := c.Sub.GetBlockTime(ctx, vLog)
 	if err != nil {
-		log.Error().Err(err).Msg("failed to retrieve timestamp")
+		log.Error().Err(err).Msg("failed to retrieve block timestamp")
 	}
-	ts := common.UnixToTimestampPb(int64(t * 1000))
 
-	return contract.Message(vLog, ts)
+	var sender *ethcommon.Address
+	var receiver *ethcommon.Address
+
+	tx, err := c.Sub.TransactionByHash(ctx, vLog.TxHash)
+	if err != nil {
+		log.Error().Err(err).Str("txHash", vLog.TxHash.String()).Msg("failed to retrieve transaction by hash")
+	} else {
+		receiver = tx.To()
+		adrr, err := c.Sub.TransactionSender(ctx, tx, vLog.BlockHash, vLog.TxIndex)
+		if err != nil {
+			log.Error().Err(err).Str("txHash", vLog.TxHash.String()).Msg("failed to retrieve transaction sender")
+		} else {
+			sender = &adrr
+		}
+	}
+
+	return contract.Message(Log{
+		Log:             vLog,
+		BlockTime:       time.Unix(int64(ts), 0),
+		SenderAddress:   sender,
+		ReceiverAddress: receiver,
+	})
 }
