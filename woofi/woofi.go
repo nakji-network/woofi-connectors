300da0e9c300bd562f6b5f773e8739133db8724d
Doan Phi Hung <davidhung@blep.ai>
2022-12-19 10:37:50 +0700

    Define constant for timeout duration

--- woofi/woofi.go
+++ woofi/woofi.go
@@ -16,6 +16,8 @@ import (
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
+const TimeoutDuration = 5 * time.Minute
+
 type Config struct {
 	NetworkName string
 	FromBlock   uint64
@@ -124,7 +126,7 @@ func (c *Connector) backfill(addresses []ethcommon.Address) {
 }
 
 func (c *Connector) parse(contract ISmartContract, vLog types.Log) (protoreflect.ProtoMessage, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), TimeoutDuration)
 	defer cancel()
 
 	ts, err := c.getBlockTime(ctx, vLog)
